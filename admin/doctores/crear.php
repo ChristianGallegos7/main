16bb6fe258174819a7f2c73e0372d23a2072445c
unknown <chris@chris.com>
2023-12-19 00:02:54 -0500

    opa2

--- admin/doctores/crear.php
+++ admin/doctores/crear.php
@@ -1,16 +1,91 @@
 <?php
     include "../templates/header.php";
+    //base de datos
+    include "../../includes/config/database.php";
+
+    $conexion = conectarDb();
+
+    //seleccionar la especialidad 
+    $query = "SELECT * FROM tbl_especialidades";
+    $resultado = mysqli_query($conexion,$query);
+    //arreglo de errores
+    $errores = [];
+
+    $nombre = '';
+    $apellido = '';
+    $especialidad = '';
+    $horario = '';
+    $foto = '';
+
+    
+
+    if($_SERVER['REQUEST_METHOD']== "POST"){
+
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $especialidad = $_POST['especialidad'];
+        $horario = $_POST['horario'];
+
+        //imagen 
+        $foto = $_FILES['foto'];
+       
+        if(!$nombre){
+            $errores[] = "El nombre es requerido";
+        }
+
+        if(!$apellido){
+            $errores[] = "El apellido es requerido";
+        }
+
+        if(!$especialidad){
+            $errores[] = "la especialidad es requerido";
+        }
+        if(!$horario){
+            $errores[] = "El horario es requerido";
+        }
+
+        if(!$foto['name']){
+            $errores[] = "La foto es requerida";
+        }
+
+        if(empty($errores)){
+
+            //SUBIR ARCHIVOS
+
+            //CREEAAMOS LAA CARPETAA
+            $carpetaImagenes = "../../imagenes";
+
+            //si la carpeta no existe la crea
+            if(!is_dir($carpetaImagenes)){
+                mkdir($carpetaImagenes);
+            }
+
+            //query para insertarwd
+            $doctor = "INSERT INTO tbl_doctores(nombre,apellido,especialidad,horario,foto) VALUES('$nombre','$apellido','$especialidad','$horario', '$foto')";
+
+            //insertar en la base
+            $result = mysqli_query($conexion,$doctor);
+
+            if($result){
+                //redireccionar si se inserto correctamente el doctor en la base de datos
+                header('Location: index.php');
+            }
+        }
+    }
+
 ?>
     <main>
         <h1>Crear Doctor</h1>
 
-        <form action="" method="POST" enctype="" class="formulario">
+        <?php foreach($errores as $error):?>
+            <div>
+                <?php echo $error;?>
+            </div>
+        <?php endforeach;?>    
+        <form action="" method="POST" enctype="multipart/form-data" class="formulario">
             <fieldset>
                 <legend>Información General</legend>
-                <div>
-                    <label for="foto">Foto:</label>
-                    <input type="file" name="foto" id="foto">
-                </div>
+                
                 <div>
                     <label for="nombre">Nombre:</label>
                     <input type="text" name="nombre" id="nombre">
@@ -21,12 +96,22 @@
                 </div>
                 <div>
                     <label for="especialidad">Especialidad:</label>
-                    <input type="text" name="especialidad" id="especialidad">
-
+                    <select name="especialidad" id="especialidad">
+                        <option value="">Seleccione una especialdiad por Doctor</option>
+                        <?php while($row = mysqli_fetch_assoc($resultado)):?>
+                            <option value="<?php echo $row['id'];?>" <?php echo $especialidad === $row['id'] ? 'selected': '' ?>> 
+                                <?php echo $row['nombre'];?> 
+                            </option>
+                        <?php endwhile;?>   
+                    </select>
                 </div>
                 <div>
                     <label for="horario">Horario:</label>
                     <input type="text" name="horario" id="horario">
+                </div>
+                <div>
+                    <label for="foto">Foto:</label>
+                    <input type="file" name="foto" id="foto">
                 </div>
 
                 <button type="submit" class="btn-verde">Añadir Doctor</button>
--- registro.php
+++ registro.php
@@ -1,9 +1,20 @@
 <?php
-    //base de datos
- include "./includes/config/database.php";
- $conexion = conectarDb();
+//base de datos
+include "./includes/config/database.php";
+$conexion = conectarDb();
 
- if($_SERVER['REQUEST_METHOD'] == "POST"){
+//arreglo de errores
+$errores = [];
+
+$nombre = '';
+$apellido = '';
+$cedula = '';
+$telefono = '';
+$correo = '';
+$contrasena = '';
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $cedula = $_POST['cedula'];
@@ -11,58 +22,87 @@
     $correo = $_POST['correo'];
     $contrasena = $_POST['contrasena'];
 
+    if (!$nombre) {
+        $errores[] = "El nombre es obligatorio";
+    }
+    if (!$apellido) {
+        $errores[] = "El apellido es obligatorio";
+    }
+    if (!$cedula) {
+        $errores[] = "La cedula es obligatorio";
+    }
+    if (!$telefono) {
+        $errores[] = "El telefono es obligatorio";
+    }
+    if (!$correo) {
+        $errores[] = "El correo es obligatorio";
+    }
+    if (!$contrasena) {
+        $errores[] = "La contraseña es obligatorio";
+    }
+
     //hash a la contraseña
     $contrasenaHash = password_hash($contrasena, PASSWORD_BCRYPT);
 
-    //query para insertar en la base
-    $query = "INSERT INTO tbl_usuarios(nombre,apellido,cedula,telefono,correo,contrasena) VALUES('$nombre','$apellido','$cedula','$telefono','$correo','$contrasenaHash')";
+    if (empty($errores)) {
+        //query para insertar en la base
+        $query = "INSERT INTO tbl_usuarios(nombre,apellido,cedula,telefono,correo,contrasena) VALUES('$nombre','$apellido','$cedula','$telefono','$correo','$contrasenaHash')";
 
-    $resultado = mysqli_query($conexion,$query);
+        $resultado = mysqli_query($conexion, $query);
 
 
-    if($resultado){
-        echo "Se inserto correctamene";
-    }else{
-        echo "no se inserto";
+        if ($resultado) {
+            echo "Se inserto correctamene";
+        } else {
+            echo "no se inserto";
+        }
     }
- }
- //cerrar la conexion
- mysqli_close($conexion);
+}
+//cerrar la conexion
+mysqli_close($conexion);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
 </head>
+
 <body>
 
     <main>
+        <?php foreach($errores as $error):?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach;?>    
         <form action="registro.php" method="POST">
             <label for="nombre">Nombre:</label>
-            <input type="text" id="nombre" name="nombre" required>
+            <input type="text" id="nombre" name="nombre" value="<?php echo $nombre;?>" >
 
             <label for="apellido">Apellido:</label>
-            <input type="text" id="apellido" name="apellido" required>
+            <input type="text" id="apellido" name="apellido" value="<?php echo $apellido;?>" >
 
             <label for="cedula">Cédula:</label>
-            <input type="text" id="cedula" name="cedula" required>
+            <input type="text" id="cedula" name="cedula" value="<?php echo $cedula;?>" >
 
             <label for="telefono">Teléfono:</label>
-            <input type="text" id="telefono" name="telefono" required>
+            <input type="text" id="telefono" name="telefono" value="<?php echo $telefono;?>" >
 
             <label for="correo">Correo:</label>
-            <input type="email" id="correo" name="correo" required>
+            <input type="email" id="correo" name="correo" value="<?php echo $correo;?>" >
 
             <label for="contrasena">Contraseña:</label>
-            <input type="password" id="contrasena" name="contrasena" required>
+            <input type="password" id="contrasena" name="contrasena" value="<?php echo $contrasena;?>" >
 
             <button type="submit">Registrarse</button>
         </form>
     </main>
 
 </body>
+
 </html>
